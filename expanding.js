3d31744e5721bfef37a381e39fa1b594218295d6
markalexa <mark.alexa.uk@gmail.com>
2017-08-05 09:23:11 +0100

    adding expanding.js jquery file

---
+++ expanding.js
@@ -0,0 +1,24 @@
+$("#in").mouseenter(function() {
+		$(this).animate({
+			width:"40px",
+			height:"auto"},
+			 1000);
+		});
+		$("#in").mouseleave(function() {
+		$(this).animate({
+			width:"30px",
+			height:"auto"}, 
+			1000);
+		});
+		$("#gplus").mouseenter(function() {
+		$(this).animate({
+			width:"42px",
+			height:"auto"},
+			 1000);
+		});
+		$("#gplus").mouseleave(function() {
+		$(this).animate({
+			width:"32px",
+			height:"auto"}, 
+			1000);
+		});
