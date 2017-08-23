4bff4f62feb6401f3f16bae1d756980a7d8011e1
markalexa <mark.alexa.uk@gmail.com>
2017-08-23 21:41:13 +0100

    editing messageSent.js file

--- messageSent.js
+++ messageSent.js
@@ -28,7 +28,7 @@ function messageSent() {
     	context.strokeStyle = "#21a527";
     	context.stroke();
     	context.fillStyle = "#21a527";
-    	context.font = "19px Verdana";
+    	context.font = "15px Verdana";
     	context.strokeText("Great ! Message has been sent.",80,110);
     	context.strokeText("I'll get back to you as soon as I can.",80,130);
     	context.strokeText("Have a good one !",80,150);
