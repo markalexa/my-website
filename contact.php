7cca331c33119545e8f2f1df26cbfc970a8ac400
markalexa <mark.alexa.uk@gmail.com>
2017-09-30 23:30:58 +0100

    massive update

--- contact.php
+++ contact.php
@@ -8,8 +8,14 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
-	 <title>Mark Alexa: Contact</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+     integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+    integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <title>Mark Alexa: Contact</title>
 	 <link rel="stylesheet" href="contact.css" type="text/css">
     
   
@@ -17,11 +23,11 @@
   <body>
  
 <!-- NAVIGATION BAR -->
-<nav class="navbar navbar-default navbar-fixed-top">
+<nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
@@ -39,11 +45,11 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
          
-      <ul class="nav navbar-nav navbar-right">
-       	 <li><a id="hover" href="index.html">Home</a></li>
-          <li><a id="hover" href="about.html">Who I am</a></li>
-          <li><a id="hover" href="my_work.html">My Work</a></li>
-          <li><a id="hover" href="contact.php">Contact Me</a></li>
+      <ul class="nav navbar-nav navbar-right" id="littleBitDown">
+       	 <li><a href="index.html"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>&nbsp;Home</a></li>
+          <li><a href="about.html" alt="about me"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;Who I am</a></li>
+          <li><a href="my_work.html" alt="my work"><span class="glyphicon glyphicon-wrench" aria-hidden="true"></span>&nbsp;My Work</a></li>
+        	 <li class="active"><a href="contact.php" alt="contact me"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>&nbsp;Contact Me</a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
@@ -86,8 +92,6 @@
 	</div>
    </div>
    
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-   <script src="bootstrap/js/bootstrap.min.js"></script>
    <script src="expanding.js" type="text/javascript"></script>
    <script type="text/javascript">
    	var windowWidth = $(window).width();
